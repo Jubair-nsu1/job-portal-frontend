2f7361657ffb5c4ed0aef9989a1a5a355565f0fc
Jubair Ahmed <62503637+Jubair-nsu1@users.noreply.github.com>
2024-11-10 18:52:25 +0600

    Update helper.js

--- src/services/helper.js
+++ src/services/helper.js
@@ -1,4 +1,4 @@
 //export const SERVER_URL = "http://career.bylc.org:4000"
 //export const SERVER_URL = "http://localhost:4000"
-export const SERVER_URL = "https://job-portal-backend-05l8.onrender.com"
+export const SERVER_URL = "https://job-portal-backend-05l8.onrender.com:4000"
 // export const SERVER_URL = process.env.URL;
